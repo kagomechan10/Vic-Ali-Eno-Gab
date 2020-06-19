6fa1557213dd36050887d8f5e881f112aee171ce
Alice <bleusealice1@gmail.com>
2020-06-19 16:35:16 +0200

    Menu

---
+++ Assets/Script/PlayButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+
+public string loadLevel;
+
+public void startGame(){
+
+    Application.LoadLevel (loadLevel);
+
+   }
+
+public void quitGame(){
+
+    Application.Quit ();
+}
+}
